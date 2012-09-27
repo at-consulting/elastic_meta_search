1e738c9f01433cf0c4c77cceec4d7cf6131d9994
Donskoy D <ddonskoi@at-consulting.ru>
2012-09-27 13:04:21 +0400

    Fix for situation when controller is not inherited from InheritedResource::Base (Developer can define name of index by himself)

--- lib/elastic_meta_search/view_helpers.rb
+++ lib/elastic_meta_search/view_helpers.rb
@@ -1,7 +1,7 @@
 #coding: utf-8
 module ElasticMetaSearch
   module ViewHelpers
-    
+
     def search_bar(opts = {})
       content_tag :div, class: "search input-append" do
         elastic_search_bar(opts[:elastic_search] || {}) + meta_search_link(opts[:meta_search])
@@ -10,10 +10,10 @@ module ElasticMetaSearch
 
     def elastic_search_bar(opts)
       url_index   = opts.delete(  :index) || resource_class.model_name.underscore.pluralize
-      url_display = opts.delete(:display) || ElasticMetaSearch.indexes[resource_class].keys
+      url_display = opts.delete(:display) || url_index.classify.constantize.mapping.keys
       source      = fs_path(index: url_index, display: url_display)
       text_val    = params[:fs][:term] if params[:fs]
-      
+
       form_tag url_for(controller: controller_name, action: action_name), method: :get, style: "display:inline;" do |f|
         text_field_tag("fs[term]", text_val, class: "span4", data: { source: source }) +
         button_tag("Найти", disable_with: "Поиск...", class: "btn")
