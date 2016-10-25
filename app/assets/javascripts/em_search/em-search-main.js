4ae7a813e560d4b5a345027c439eab2ccb7dea6d
Denis Tataurov <sineedus@mail.ru>
2016-10-25 22:17:52 +0300

    Styling fixes

--- app/assets/javascripts/em_search/em-search-main.js
+++ app/assets/javascripts/em_search/em-search-main.js
@@ -19,12 +19,12 @@ $(function(){
       var t = item.label.replace(re,"<b>$1</b>");
       var infoSpanWidth = function(){
         var maxWidth = "";
-        ul.find('li span.info').each( function(el){
+        ul.find('li span.founded-parameter').each( function(el){
           if(maxWidth < el.attr('width'))
             maxWidth = el.attr('width')
         });
       };
-      var renderContent = $("<a><span class='value'>" + t + "</span><span class='info label label-info'>" + item.column + "</span></a>");
+      var renderContent = $("<a><span class='value'>" + t + "</span><span class='founded-parameter label label-info'>" + item.column + "</span></a>");
       return $("<li></li>")
          .data("item.autocomplete", item)
          .append( renderContent )
@@ -37,14 +37,14 @@ $(function(){
       var  infoWidthAry = [];
       ul.find("li").each(function(){
         valueWidthAry.push($(this).find("span.value").width());
-        infoWidthAry.push($(this).find("span.info").outerWidth(true));
+        infoWidthAry.push($(this).find("span.founded-parameter").outerWidth(true));
       });
       var liA = ul.find("li a").first();
       ul.width( Math.max(
         (Math.max.apply(null, valueWidthAry) + Math.max.apply(null, infoWidthAry) + liA.outerWidth(true) - liA.width() + 1),
         this.element.outerWidth()
       ) );
-      ul.find("li span.info").each(function(){
+      ul.find("li span.founded-parameter").each(function(){
         $(this).outerWidth(Math.max.apply(null, infoWidthAry))
       });
     };
